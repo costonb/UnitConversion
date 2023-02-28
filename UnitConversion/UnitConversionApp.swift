556a54f90c3111a89525f3efe618670472277b70
Brandon Coston <costonb@gmail.com>
2023-02-27 23:26:12 -0800

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Brandon Coston on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
